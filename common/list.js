3e0f91b22e490ca0872e8b643a2a262b0302a287
Cristian Oliveira <cristian.oliveira@miew.pt>
2023-10-21 12:36:16 +0100

    chore: documentation

--- common/list.js
+++ common/list.js
@@ -2,7 +2,7 @@ const exercisesData = [
 	{ href: "exercises/0-1-hello-world", name: "0-1-hello-world", category: "html" },
 	{ href: "exercises/0-2-newspaper-article", name: "0-2-newspaper-article", category: "html" },
 	{ href: "exercises/0-3-html-table", name: "0-3-html-table", category: "html" },
-	// { href: "exercises/0-4-newsletter-table", name: "0-4-newsletter-table", category: "html" },
+	 { href: "exercises/0-4-newsletter-table", name: "0-4-newsletter-table", category: "html" },
 	// { href: "exercises/0-5-html-final", name: "0-5-html-final", category: "html" },
 	// { href: "exercises/1-0-css-design-systems", name: "1-0-css-design-systems", category: "css" },
 	// { href: "exercises/1-1-css-navy-battle", name: "1-1-css-navy-battle", category: "css" },
