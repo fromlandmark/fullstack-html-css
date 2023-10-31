5a832205cfa6ca639860bb012071988b1507879c
Cristian Oliveira <cristian.oliveira@miew.pt>
2023-10-31 17:40:47 +0000

    chore: update presentations

--- common/list.js
+++ common/list.js
@@ -6,11 +6,11 @@ const exercisesData = [
 	// { href: "exercises/0-5-html-final", name: "0-5-html-final", category: "html" },
 	// { href: "exercises/1-0-css-design-systems", name: "1-0-css-design-systems", category: "css" },
 	// { href: "exercises/1-1-css-navy-battle", name: "1-1-css-navy-battle", category: "css" },
-	// { href: "exercises/1-2-sports-card", name: "1-2-sports-card", category: "css" },
+  { href: "exercises/1-2-sports-card", name: "1-2-sports-card", category: "css" },
 	// { href: "exercises/1-3-mondrian", name: "1-3-mondrian", category: "css" },
 	// { href: "exercises/1-4-animations", name: "1-4-animations", category: "css" },
 	// { href: "exercises/1-4-css-functions", name: "1-4-css-functions", category: "css" },
-	// { href: "exercises/1-5-landing-page", name: "1-5-landing-page", category: "css" },
+	  { href: "exercises/1-5-landing-page", name: "1-5-landing-page", category: "css" },
 	// { href: "exercises/2-1-loja", name: "2-1-loja", category: "css" },
 ];
 export default exercisesData;
--- common/presentationsData.js
+++ common/presentationsData.js
@@ -6,7 +6,8 @@ const presentationsData = [
 	},
 	{ path: "presentations/0-3-html-medias-2023.pdf", name: "0-3-html-medias.pdf" },
 	{ path: "presentations/0-4-html-blocks-tables-2023.pdf", name: "0-4-html-blocks-tables.pdf" },
-	{ path: "presentations/css-introducao-2023", name: "css-introducao" },
-	{ path: "presentations/css-properties", name: "css-introducao-2" },
+	{ path: "presentations/css-introducao-2023.pdf", name: "css-introducao" },
+	{ path: "presentations/css-properties.pdf", name: "css-introducao-2" },
+	{ path: "presentations/flex.pdf", name: "css-flex" },
 ];
 export default presentationsData;
